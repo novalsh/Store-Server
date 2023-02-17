9773d4e43d85d23563738b37084256684a9b9991
novalsh <muhammad.harjana@binus.ac.id>
2023-02-18 01:24:12 +0700

    membuat config dan model product

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8000 || process.env.PORT;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+
+  .then((result) => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to database", err);
+    process.exit();
+  });
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "WELCOME TO STORE",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ app/models/index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const dbConfig = require("../../config/db.config.js");
+
+mongoose.Promise = global.Promise;
+mongoose.set('strictQuery', false);
+
+const db = {}
+db.mongoose = mongoose;
+db.url = dbConfig.url; 
+db.products = require("./product.model.js")(mongoose)
+
+module.exports = db
