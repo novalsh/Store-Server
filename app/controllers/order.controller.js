85a5b6c83be4eae18dc71474b8f7d72708fb46e9
novalsh <muhammad.harjana@binus.ac.id>
2023-02-18 13:47:46 +0700

    menambahkan item product dalam order

--- app/controllers/order.controller.js
+++ app/controllers/order.controller.js
@@ -23,12 +23,37 @@ exports.findOrder = (req, res) => {
   ])
     //then and catch
     .then((result) => {
-        res.send(result);
-        }
-    )
-    .catch((err) => {
-        res.status(500).send({
-            message: err.message || "Some error occurred while retrieving orders.",
-        });
+      res.send(result);
     })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving orders.",
+      });
+    });
+};
+
+exports.addToCart = (req, res) => {
+  const id = Number(req.params.id);
+  const productCode = String(req.body.product);
+
+  Order.updateOne(
+    {
+      user_id: id,
+    },
+    {
+      //melakukan query di dalam array miliki carditems dengan kondisi nilai tidak boleh sama
+      //fungsi dari add to set sebuah metode dalam MongoDB yang digunakan untuk menambahkan elemen ke dalam sebuah array di dalam dokumen koleksi MongoDB, namun hanya jika elemen tersebut belum ada dalam array tersebut. Jika elemen yang ditambahkan sudah ada dalam array, maka operasi akan diabaikan.
+      $addToSet: {
+        cart_items: productCode,
+      },
+    }
+  )
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving orders.",
+      });
+    });
 };
